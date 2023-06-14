709cd8ae90e82b7b4744c2575bc383d2ea719b8a
Isabela Costa <isa-hcosta@hotmail.com>
2023-06-14 10:50:47 -0300

    refatorando CategoryService

--- src/cli/CategoryService.js
+++ src/cli/CategoryService.js
@@ -1,29 +1,26 @@
-class CategoryService{
+const url = "http://localhost:3000/categories";
 
-    static async findCategories(){
-        const response = await fetch("http://localhost:3000/categories");
-        const status = response.status;
+async function resposta(response){
+    const status = response.status;
         if (status === 200){
          const json = await response.json();
-        console.log(`response status: ${status}`)
+         console.log(`response status: ${status}`)
          console.log(json);   
         } else {
             console.log(`response status: Erro ${status}!!`);
         } 
+}
+class CategoryService {
+
+    static async findCategories(){
+        const response = await fetch(url);
+        await resposta(response);
     }
 
     static async findCategoryById(id){
-        const response = await fetch(`http://localhost:3000/categories/${id}`);
-        const status = response.status;
-        if (status === 200){
-         const json = await response.json();
-        console.log(`response status: ${status}`)
-         console.log(json);   
-        } else {
-            console.log(`response status: Erro ${status}!!`);
-        } 
-    }
-
+        const response = await fetch(`${url}/${id}`);
+        await resposta(response);
+    }    
 }
 
 export default CategoryService;
